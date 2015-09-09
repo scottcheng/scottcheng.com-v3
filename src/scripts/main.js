64ef471e151f9360b21a80128ed68cd1f287797b
Scott Cheng <me@scottcheng.com>
2015-09-09 18:00:42 -0400

    Full-screen image modals

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,4 +16,13 @@ $(() => {
         $splash.removeClass(className);
       });
   });
+
+  const $projects = $('#projects');
+  $projects
+    .on('click', '.image', function() {
+      $(this).closest('.project').find('.image-fullscreen').fadeIn(250);
+    })
+    .on('click', '.image-fullscreen', function() {
+      $(this).fadeOut(250);
+    });
 });
