597a44755007745db0fde02b2159ccf588a0cfd3
Scott Cheng <me@scottcheng.com>
2015-09-09 12:50:33 -0400

    Basic portfolio

--- src/views/data.js
+++ src/views/data.js
@@ -1,73 +1,75 @@
 var marked = require('marked');
 
 var projects = (function() {
-  var sparkl = {
+  return [
+  {
     id: 'sparkl',
     name: 'Sparkl',
     description: 'Virtual brainstorming for creative collaboration.',
     link: 'https://sparkl.us',
-    type: 'web-app',
-  };
-
-  var cropit = {
+    linkText: 'Launch site',
+    type: 'Web app',
+  },
+  {
     id: 'cropit',
     name: 'cropit',
-    description: 'A "customizable crop and zoom" jQuery plugin',
+    description: 'A "customizable crop and zoom" jQuery plugin.',
     link: 'http://scottcheng.github.io/cropit/',
-    type: 'code',
-  };
-
-  var conversive = {
+    linkText: 'See demo',
+    type: 'Code',
+  },
+  {
     id: 'conversive',
     name: 'Conversive',
     description:
       'Bringing real-time classroom-style discussion to MOOC. '
         + 'Prototype validated on [a popular Coursera course](https://www.coursera.org/course/algo) with real MOOC learners.',
-    type: 'web-prototype',
-  };
-
-  var cofound = {
+    link: '/assets/projects/conversive.pdf',
+    linkText: 'Read paper',
+    type: 'Academic / web prototype',
+  },
+  {
     id: 'cofound',
     name: 'Co.found',
     description:
       'Cofounder networking platform for the Stanford community. '
       + 'Class project for [CS147](http://hci.stanford.edu/cs147/).',
     link: 'https://co-found.herokuapp.com/',
-    type: 'web-app',
-  };
-
-  var weibo = {
+    linkText: 'Launch app',
+    type: 'Mobile web app',
+  },
+  {
     id: 'weibo',
     name: 'Weibo Visualization',
     description: 'An experimental visualization of a Weibo dataset, trying to illustrate the time-wise evolution of topics.',
     link: 'http://scottcheng.github.com/weibo-time-vis/',
-    type: 'vis',
-  };
-
-  var bjAir = {
+    linkText: 'Open visualization',
+    type: 'Visualization',
+  },
+  {
     id: 'bj-air',
     name: 'Visualization of Beijing Air Pollution',
     description: 'This interactive visualization demonstrates the intensity distribution of several major pollutants in Beijing over two weeks.',
     link: 'http://scottcheng.github.com/bj-air-vis/',
-    type: 'vis',
-  };
-
-  var css3 = {
+    linkText: 'Open visualization',
+    type: 'Visualization',
+  },
+  {
+    id: 'd3101',
+    name: 'D3.js 101',
+    description: 'A technical intro to D3.js.',
+    link: 'http://scottcheng.github.io/d3js-101/',
+    linkText: 'See slides',
+    type: 'Talk',
+  },
+  {
     id: 'css3',
     name: 'The Revolutionary CSS3',
     description: 'A non-technical intro to CSS level 3.',
     link: 'http://scottcheng.github.com/revolutionary-css3/',
-    type: 'talk',
-  };
-
-  return [
-    sparkl,
-    cropit,
-    conversive,
-    cofound,
-    weibo,
-    bjAir,
-    css3,
+    linkText: 'See slides',
+    type: 'Talk',
+  },
   ].map(function(p) {
     p.description = marked(p.description);
     return p;
